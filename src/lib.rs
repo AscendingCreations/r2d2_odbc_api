cd64b03131a0cc88e47e3e4985bb40d11424fe60
Markus Klein <markus-klein@live.de>
2021-10-03 19:39:03 +0200

    Use `promote_to_send` for connection_wrapper_type

--- src/lib.rs
+++ src/lib.rs
@@ -24,9 +24,7 @@ pub struct ODBCConnectionManager {
     connection_string: String,
 }
 
-pub struct ODBCConnection(Connection<'static>);
-
-unsafe impl Send for ODBCConnection {}
+pub struct ODBCConnection(odbc_api::force_send_sync::Send<Connection<'static>>);
 
 impl ODBCConnection {
     pub fn raw(&self) -> &Connection<'static> {
@@ -134,9 +132,16 @@ impl r2d2::ManageConnection for ODBCConnectionManager {
 
     fn connect(&self) -> std::result::Result<Self::Connection, Self::Error> {
         let env = &ENV;
-        Ok(ODBCConnection(
-            env.connect_with_connection_string(&self.connection_string)?,
-        ))
+        let conn = env.connect_with_connection_string(&self.connection_string)?;
+        // Promoting a connection to send is unsafe, since not every ODBC driver is thread safe.
+        // Actual thread safety for unixODBC may also depend on the threading level defined for the
+        // ODBC driver. Here we assume that the user conciously checked the safety of the
+        // application and checked into sending connection then the ODBConnectionManager has been
+        // constructed.
+        let conn = unsafe {
+            conn.promote_to_send()
+        };
+        Ok(ODBCConnection(conn))
     }
 
     fn is_valid(&self, conn: &mut Self::Connection) -> std::result::Result<(), Self::Error> {
